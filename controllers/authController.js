de517b70bf2adfb3a77d9b2b04a4b057fcf14c98
anetten <anet.zvekova@gmail.com>
2024-03-18 17:43:01 +0200

    hw 4

---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+import jwt from "jsonwebtoken";
+
+import * as authService from "../services/authService.js";
+
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
+
+import HttpError from "../helpers/HttpError.js";
+
+const { JWT_SECRET } = process.env;
+
+const signup = async (req, res) => {
+  const { email } = req.body;
+  const user = await authService.findUser({ email });
+  if (user) {
+    throw HttpError(409, "Email in use");
+  }
+  const newUser = await authService.signup(req.body);
+
+  res.status(201).json({
+    username: newUser.username,
+    email: newUser.email,
+  });
+};
+
+const login = async (req, res) => {
+  const { email } = req.body;
+  const user = await authService.findUser({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password valid");
+  }
+  const comparePassword = await authService.validatePassword(
+    password,
+    user.password
+  );
+  if (!comparePassword) {
+    throw HttpError(401, "Email or password valid");
+  }
+
+  const { _id: id } = user;
+
+  const payload = {
+    id,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+  await authService.updateUser({ _id: id }, { token });
+  res.json({ token });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await authService.updateUser({ _id }, { token: "" });
+
+  res.json({
+    message: "Logout success",
+  });
+};
+
+export default {
+  signup: ctrlWrapper(signup),
+  login: ctrlWrapper(login),
+};
